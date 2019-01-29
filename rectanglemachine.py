b70c2bfb55ede542e12862e24396ba9b7a9df706
millerbrian454 <34964641+millerbrian454@users.noreply.github.com>
2019-01-29 10:04:16 -0800

    Add files via upload
    A program that finds the area and perimeter of a rectangle.

---
+++ rectanglemachine.py
@@ -0,0 +1,9 @@
+print("What is the length?")
+length = int(input())
+print("What is the Width?")
+width = int(input())
+area = length * width
+perimeter = 2 * length + 2 * width
+print("The area is", area)
+print("The perimeter is", perimeter)
+exit()
